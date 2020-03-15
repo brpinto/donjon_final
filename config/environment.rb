8649b077ac422bda37bd05efbbadcb630312bb16
brpinto <brpinto@student.42.fr>
2020-03-15 21:14:02 +0400

    set sendgrid config

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-    :user_name => Rails.application.credentials.sg_username,
-    :password => Rails.application.credentials.sg_password,
-    :domain => 'www.donjonsetcornichons.fr',
-    :address => 'smtp.sendgrid.net',
-    :port => 465,
-    :authentication => :plain,
-    :enable_starttls_auto => true,
-    :only_path => true
-}
