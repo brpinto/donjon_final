0eee51fedcf4f08a1742f16604af630fd1822a88
brpinto <brpinto@student.42.fr>
2020-03-28 15:03:41 +0400

    add condition on table entry update

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,14 +1,21 @@
 class StatsController < ApplicationController
 
   def index
-    current_month = Date.today.mon
-    @stats = Stat.find(current_month)
+    current_time = Time.now
+    current_year = current_time.year
+    current_month = current_time.month
+
     @users_count = User.count
     @notes_count = Note.count
     @confirmed_count = User.where('confirmed = 1').count
     @notes_per_user = @notes_count / @users_count
 
-    @stats.update(users_total: @users_count, notes_total: @notes_count, users_confirmed: @confirmed_count, notes_per_user: @notes_per_user)
+    if @stat = Stat.where('YEAR(created_at) = :year AND MONTH(created_at) = :month', year: current_year, month: current_month).first
+      @stat.update(users_total: @users_count, notes_total: @notes_count, users_confirmed: @confirmed_count, notes_per_user: @notes_per_user)
+    else
+      @stat = Stat.new
+      @stat.update(users_total: @users_count, notes_total: @notes_count, users_confirmed: @confirmed_count, notes_per_user: @notes_per_user)
+    end
 
     render_json
   end
@@ -21,7 +28,7 @@ class StatsController < ApplicationController
   def render_json
     render json: {
         months_name: ["Janv", "Fév", "Mars", "Avr", "Mai", "Juin", "Juil", "Août", "Sept", "Oct", "Nov", "Déc"],
-        months_data: Stat.all
+        months_data: Stat.all.order('created_at ASC')
     }
   end
 end
